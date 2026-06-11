2111fe4dc5e4e349d5e888a93e5945b386aa1ff6
宗z <www.yaozong@foxmail.com>
2026-06-11 20:16:50 +0800

    port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,11 +8,11 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "http://localhost:8010",
         changeOrigin: true,
       },
       "/health": {
-        target: "http://localhost:8000",
+        target: "http://localhost:8010",
         changeOrigin: true,
       },
     },
